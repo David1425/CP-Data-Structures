9c388784c8f6b27d820970e78f1d937d0f1f44eb
David1425 <56770949+David1425@users.noreply.github.com>
2021-09-20 21:14:08 -0400

    Create prime_sieve.cpp

---
+++ Primes/prime_sieve.cpp
@@ -0,0 +1,36 @@
+class prime_sieve {
+    public:
+    prime_sieve() {}
+    prime_sieve(int n) { init(n); }
+    
+    vector<int> lowest_prime_factor;
+    vector<bool> is_prime;
+    void init(int n) {
+        lowest_prime_factor.resize(n+5);
+        is_prime.assign(n+5, 1);
+        is_prime[1] = 0;
+        for (int i = 1; i <= n; i++) lowest_prime_factor[i] = i;
+        for (long long i = 2; i*i <= n; i++) {
+            if (is_prime[i]) {
+                for (long long j = i*i; j <= n; j += i) {
+                    if (is_prime[j]) {
+                        is_prime[j] = 0;
+                        lowest_prime_factor[j] = i;
+                    }
+                }
+            }
+        }
+    }
+    
+    int lps(int x) { return lowest_prime_factor[x]; }
+    bool check(int x) { return is_prime[x]; }
+    
+    vector<int> factor(int x) {
+        vector<int> fac;
+        while (x > 1) {
+            fac.push_back(lps(x));
+            x /= lps(x);
+        }
+        return fac;
+    }
+};
