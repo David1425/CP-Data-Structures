eca12d0343530990859beb0b05e83fe1d2f93b80
David1425 <56770949+David1425@users.noreply.github.com>
2021-09-25 17:28:25 -0400

    Update sparse_table.cpp

--- Others/sparse_table.cpp
+++ Others/sparse_table.cpp
@@ -6,7 +6,7 @@ class sparse_table {
     public:
     vector<vector<T>> tbl;
     int n;
-    sparse_table(int mx=0) : n(mx) { tbl.assign(mx, vector<T>(log2(mx))); }
+    sparse_table(int mx=0) : n(mx) { tbl.assign(mx, vector<T>(log2(mx)+1)); }
     void build(vector<T> vec) {
         if (n == 0) n = vec.size();
         for (int i = 0; i < n; i++) tbl[i][0] = vec[i];
