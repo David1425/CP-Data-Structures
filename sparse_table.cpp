00a29999176b7ddeb42914ba35ea1327b48c4dbb
David1425 <56770949+David1425@users.noreply.github.com>
2021-09-25 16:53:39 -0400

    Create sparse_table.cpp

---
+++ sparse_table.cpp
@@ -0,0 +1,24 @@
+template<typename T=int>
+class sparse_table {
+    int log2(long long x) { return 63 - __builtin_clzl(x); }
+    int log2(int x) { return 31 - __builtin_clz(x); }
+    
+    public:
+    vector<vector<T>> tbl;
+    int n;
+    sparse_table(int mx) : n(mx) { tbl.assign(mx, vector<T>(log2(mx))); }
+    void build(vector<T> vec) {
+        for (int i = 0; i < n; i++) tbl[i][0] = vec[i];
+        int l = log2(n);
+        for (int j = 1; j <= l; j++) {
+            for (int i = 0; i+(1<<(j-1)) < n; i++) {
+                tbl[i][j] = min(tbl[i][j-1], tbl[i+(1<<(j-1))][j-1]);
+            }
+        }
+    }
+    
+    T query(int l, int r) {
+        int lg = log2(r-l+1);
+        return min(tbl[l][lg], tbl[r-(1<<(lg))+1][lg]);
+    }
+};
