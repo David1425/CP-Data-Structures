14acfd3fa73ffc73d658d5b1cbb281eb005e4a9b
David1425 <56770949+David1425@users.noreply.github.com>
2021-11-28 16:08:50 -0500

    Update mod_inv.cpp

--- Math/mod_inv.cpp
+++ Math/mod_inv.cpp
@@ -1,17 +1,17 @@
-void exgcd(ll a, ll b, ll& x, ll& y) {
+void exgcd(long long a, long long b, long long& x, long long& y) {
     if (b == 0) {
         x = 1;
         y = 0;
         return;
     }
-    ll x1 = x, y1 = y;
+    long long x1 = x, y1 = y;
     exgcd(b, a%b, x1, y1);
     y = x1 - (a/b)*y1;
     x = y1;
 }
 
-ll modInv(ll a, ll m) {
-    ll x, y;
+long long modInv(long long a, long long m) {
+    long long x, y;
     if (a < m) exgcd(a, m, x, y);
     else exgcd(m, a, x, y);
     return (x%m+m)%m;
