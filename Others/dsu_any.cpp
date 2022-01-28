9974c1bcf6d7a8a18c81ea9376f750ec18fc8e9a
David1425 <56770949+David1425@users.noreply.github.com>
2022-01-28 15:04:44 -0500

    Update dsu_any.cpp

--- Others/dsu_any.cpp
+++ Others/dsu_any.cpp
@@ -20,6 +20,8 @@ class DSU {
     void merge(T a, T b) {
         T x = find(a), y = find(b);
         if (x == y) return;
+        if (!size[x]) size[x] = 1;
+        if (!size[y]) size[y] = 1;
         if (size[x] >= size[y]) par[y] = x, size[x]+=size[y];
         else par[x] = y, size[y]+=size[x];
     }
