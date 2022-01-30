1ae4cedcbbe5d8807d396ef554d3c3572201aa31
David1425 <56770949+David1425@users.noreply.github.com>
2022-01-29 21:08:50 -0500

    Update suffix_array.cpp

--- Arrays/suffix_array.cpp
+++ Arrays/suffix_array.cpp
@@ -1,47 +1,44 @@
-struct suffix_array {
-    string s;
-    int k, x;
-    
-    vector<int> ind, val;
-    int size;
-    
-    void build(string str) {
-        s = str+"$";
-        vector<pair<char, int>> vec(size);
-        for (int i = 0; i < size; i++) {
-            vec[i] = {s[i], i};
-        }
-        sort(vec.begin(), vec.end());
-        
-        ind[0] = vec[0].second;
-        val[ind[0]] = 0;
-        for (int i = 1; i < size; i++) {
-            ind[i] = vec[i].second;
-            val[ind[i]] = val[ind[i-1]];
-            if (vec[i].first != vec[i-1].first) val[ind[i]]++;
-        }
-        
-        vector<pair<pair<int, int>, int>> a(size);
-        
-        k = 0;
-        int l = log2(size);
-        while (k <= l) {
-            x = 1 << k;
-            for (int i = 0; i < size; i++) a[i] = {{val[i], val[(i+x)%size]}, i};
-            sort(a.begin(), a.end());
-            ind[0] = a[0].second;
-            val[ind[0]] = 0;
-            for (int i = 1; i < size; i++) {
-                ind[i] = a[i].second;
-                if (a[i-1].first != a[i].first) val[ind[i]]=val[ind[i-1]]+1;
-                else val[ind[i]] = val[ind[i-1]];
-            }
-            k++;
-        }
-    }
-    
-    suffix_array(int n) : size(n+1), ind(n+1), val(n+1) {}
-    suffix_array(string str) : s(str), size(int(str.size())+1), ind(int(str.size())+1), val(int(str.size())+1) { build(str); }
-    
-    string getSuffix(int i) { return s.substr(ind[i], size-ind[i]); }
-};
+vector<int> suffix_array(string s) {
+	int n = int(s.size()), m = CHAR_MAX;
+	vector<int> cnt(m), p(n), c(n);
+	for (int i = 0; i < n; i++) cnt[s[i]]++;
+	for (int i = 1; i < m; i++) cnt[i] += cnt[i-1];
+	for (int i = 0; i < n; i++) {
+		cnt[s[i]]--;
+		p[cnt[s[i]]] = i;
+	}
+	
+	c[p[0]] = 0;
+	int ind = 0;
+	for (int i = 1; i < n; i++) {
+		if (s[p[i]] != s[p[i-1]]) ind++;
+		c[p[i]] = ind;
+	}
+	ind++;
+	
+	vector<int> pt(n), ct(n);
+	for (int k = 0; (1<<k) < n; k++) {
+		for (int i = 0; i < n; i++) {
+			pt[i] = (p[i]-(1<<k)+n)%n;
+		}
+		fill(cnt.begin(), cnt.begin()+ind, 0);
+		for (int i = 0; i < n; i++) cnt[c[i]]++;
+		for (int i = 1; i < ind; i++) cnt[i] += cnt[i-1];
+		for (int i = n-1; i >= 0; i--) {
+			cnt[c[pt[i]]]--;
+			p[cnt[c[pt[i]]]] = pt[i];
+		}
+		ct[p[0]] = 0;
+		ind = 0;
+		for (int i = 1; i < n; i++) {
+			pair<int, int> cur = {c[p[i]], c[(p[i]+(1<<k))%n]};
+			pair<int, int> prev = {c[p[i-1]], c[(p[i-1]+(1<<k))%n]};
+			if (cur != prev) ind++;
+			ct[p[i]] = ind;
+		}
+		ind++;
+		c = ct;
+	}
+	
+	return p;
+}
