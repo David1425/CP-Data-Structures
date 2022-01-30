a8fd7f470fe112679d55c9c42442f34b86215e61
David1425 <56770949+David1425@users.noreply.github.com>
2022-01-29 21:49:16 -0500

    Update sparse_table.cpp

--- Arrays/sparse_table.cpp
+++ Arrays/sparse_table.cpp
@@ -1,14 +1,14 @@
 template<typename T=int>
 struct sparse_table {
-    int log2(long long x) { return 63 - __builtin_clzl(x); }
-    int log2(int x) { return 31 - __builtin_clz(x); }
+    int flog2(long long x) { return 63 - __builtin_clzl(x); }
+    int flog2(int x) { return 31 - __builtin_clz(x); }
     
     vector<vector<T>> tbl;
     int n;
     sparse_table(){}
     void build(vector<T> vec) {
         n = vec.size();
-        int lg = log2(n);
+        int lg = flog2(n);
         tbl.assign(n, vector<T>(lg+1));
         for (int i = 0; i < n; i++) tbl[i][0] = vec[i];
         for (int j = 1; j <= lg; j++) {
@@ -19,7 +19,7 @@ struct sparse_table {
     }
     
     T query(int l, int r) {
-        int lg = log2(r-l+1);
+        int lg = flog2(r-l+1);
         return min(tbl[l][lg], tbl[r-(1<<(lg))+1][lg]);
     }
 };
