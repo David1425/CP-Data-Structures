14bfdec0fa833b7d383e5e9755398c743f45dd5c
David1425 <56770949+David1425@users.noreply.github.com>
2022-02-02 10:21:13 -0500

    Update lca_dfs.cpp

--- Trees/lca_dfs.cpp
+++ Trees/lca_dfs.cpp
@@ -1,10 +1,12 @@
-vector<pair<int, int>> lca_depth;
-void lca_dfs(int u, int p, int d) {
-    lca_depth.push_back({d, u});
-    for (auto v : adj[u]) {
+vector<ll> lca_depth;
+int pos[MM];
+void lca_dfs(int u, int p, ll d) {
+    if (pos[u] == -1) pos[u] = int(lca_depth.size());
+    lca_depth.push_back(d);
+    for (auto [v, w] : adj[u]) {
         if (v != p) {
-            lca_dfs(v, u, d+1);
-            lca_depth.push_back({d, u});
+            lca_dfs(v, u, d+w);
+            lca_depth.push_back(d);
         }
     }
 }
