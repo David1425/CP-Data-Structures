93082e2f21ac790b567acf1cdb420d7d8bc88378
David1425 <56770949+David1425@users.noreply.github.com>
2022-02-04 09:19:57 -0500

    Update coodinate_compression.cpp

--- Others/coodinate_compression.cpp
+++ Others/coodinate_compression.cpp
@@ -1,4 +1,5 @@
 unordered_map<int, int> ccomp(vector<int> &vec, int start) {
+    sort(all(vec));
     vec.erase(unique(vec.begin(), vec.end()), vec.end());
     int len = vec.size();
     unordered_map<int, int> um;
