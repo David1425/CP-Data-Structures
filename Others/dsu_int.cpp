7fb19eb5c5a558364aeefd0369825131565d74a7
David1425 <56770949+David1425@users.noreply.github.com>
2022-03-20 12:54:37 -0400

    Update dsu_int.cpp

--- Others/dsu_int.cpp
+++ Others/dsu_int.cpp
@@ -1,30 +1,34 @@
 class DSU {
-    public:
-    vector<int> size;
-    vector<int> par;
-    int MX;
-    
-    DSU() {}
-    DSU(int n) : MX(n+2) {
-        size.assign(MX, 1);
-        par.assign(MX, -1);
-    }
-    
-    void clear() {
-        size.assign(MX, 1);
-        par.assign(MX, -1);
-    }
-    
-    int find(int x) {
-        if (par[x] == -1) return x;
-        par[x] = find(par[x]);
-        return par[x];
-    }
-    
-    void merge(int a, int b) {
-        int x = find(a), y = find(b);
-        if (x == y) return;
-        if (size[x] >= size[y]) par[y] = x, size[x] += size[y];
-        else par[x] = y, size[y] += size[x];
-    }
+	public:
+	vector<int> size;
+	vector<int> par;
+	int MX;
+	
+	DSU() {}
+	DSU(int n) : MX(n+2) {
+		size.assign(MX, 1);
+		par.assign(MX, -1);
+	}
+	
+	void clear() {
+		size.assign(MX, 1);
+		par.assign(MX, -1);
+	}
+	
+	int find(int x) {
+		if (par[x] == -1) return x;
+		par[x] = find(par[x]);
+		return par[x];
+	}
+	
+	bool check(int x, int y) {
+		return find(x)==find(y);
+	}
+	
+	void merge(int a, int b) {
+		int x = find(a), y = find(b);
+		if (x == y) return;
+		if (size[x] >= size[y]) par[y] = x, size[x] += size[y];
+		else par[x] = y, size[y] += size[x];
+	}
 };
