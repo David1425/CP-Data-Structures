e168b3ec29c8849bfc3a47c9e19296dda1edd054
David1425 <56770949+David1425@users.noreply.github.com>
2022-05-20 18:30:13 -0400

    Update fast_power.cpp

--- Math/fast_power.cpp
+++ Math/fast_power.cpp
@@ -1,21 +1,21 @@
 template<typename T, typename U>
 T fpow(T a, U b) {
-    T x = 1;
-    while (b > 0) {
-        if (b&1) x *= a;
-        a *= a;
-        b >>= 1;
-    }
-    return x;
+	T x = 1;
+	while (b > 0) {
+		if (b&1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
 }
 
 template<typename T, typename U, typename V>
 T fpow(T a, U b, V m) {
-    T x = 1;
-    while (b > 0) {
-        if (b&1) x = (x*a)%m;
-        a = (a*a)%m;
-        b >>= 1;
-    }
-    return x;
+	T x = 1;
+	while (b > 0) {
+		if (b&1) x = (x*a)%m;
+		a = (a*a)%m;
+		b >>= 1;
+	}
+	return x;
 }
