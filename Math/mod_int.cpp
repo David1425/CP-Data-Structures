b2465c82c358423966d72da10dd144c31681b12b
David1425 <56770949+David1425@users.noreply.github.com>
2022-05-21 07:37:16 -0400

    Create mod_int.cpp

---
+++ Math/mod_int.cpp
@@ -0,0 +1,55 @@
+template<typename intType, uint64_t mod>
+struct ModInt {
+	intType num;
+	ModInt(intType val=0) : num(val) {
+		while (num < 0) num += mod;
+		num %= mod;
+	}
+
+	using MI = ModInt<intType,mod>;
+	
+	template<typename T>
+	MI operator = (const T& val) { num=MI(val); return *this; }
+	
+	MI inv() {
+		MI a = num, x = 1;
+		uint64_t b = mod-2;
+		while (b > 0) {
+			if (b&1) x = x*a;
+			a = a*a;
+			b >>= 1;
+		}
+		return x;
+	}
+	
+	MI operator + (MI val) { return MI(num+val.num); }
+	MI operator - (MI val) { return MI(num-val.num); }
+	MI operator * (MI val) { return MI(num*val.num); }
+	MI operator / (MI val) { return MI(num*val.inv().num); }
+	
+	void operator += (MI val) { *this = (*this)+val; }
+	void operator -= (MI val) { *this = (*this)-val; }
+	void operator *= (MI val) { *this = (*this)*val; }
+	void operator /= (MI val) { *this = (*this)/val; }
+	MI& operator ++ () {
+		*this += MI(1);
+		return *this;
+	}
+	MI operator ++ (int) {
+		auto old = *this;
+		operator++();
+		return old;
+	}
+	MI& operator -- () {
+		*this -= MI(1);
+		return *this;
+	}
+	MI operator -- (int) {
+		auto old = *this;
+		operator--();
+		return old;
+	}
+	
+	friend istream& operator >> (istream& is, MI& mi) { is >> mi.num; mi=MI(mi.num); return is; }
+	friend ostream& operator << (ostream& os, const MI& mi) { os << mi.num; return os; }
+};
