08db9194bf5b2d1f5cb6a1a276ac7ac109d345d7
David1425 <56770949+David1425@users.noreply.github.com>
2022-05-22 16:06:08 -0400

    Update lca_dfs.cpp

--- Range Queries/lca_dfs.cpp
+++ Range Queries/lca_dfs.cpp
@@ -1,12 +1,12 @@
-vector<int> depth;
+vector<pair<int,int>> depth;
 int pos[MM];
-void lca_dfs(int u, int p, ll d) {
+void lca_dfs(int u, int p, int d) {
 	if (pos[u] == -1) pos[u] = int(depth.size());
-	depth.push_back(d);
-	for (auto v : adj[u]) {
-		if (v != p) {
-			lca_dfs(v, u, d+1);
-			depth.push_back(d);
+	depth.push_back({d,u});
+	for (auto x : adj[u]) {
+		if (x.v != p) {
+			lca_dfs(x.v, u, d+1);
+			depth.push_back({d,u});
 		}
 	}
 }
